6b0a4cb6fc0af3819d4310ada0ebfa27cb0ee5b0
Chris Ingrisani <71732884+chris-ingrisani@users.noreply.github.com>
2021-03-25 16:06:24 -0400

    Update gcloud_projects.rb

--- test/integration/projects/controls/gcloud_projects.rb
+++ test/integration/projects/controls/gcloud_projects.rb
@@ -197,6 +197,18 @@ control 'gcloud-projects' do
         end
       end
 
+      describe command("gcloud projects get-iam-policy #{base_project_sa[environment_code][business_unit]} --format=json") do
+        its(:exit_status) { should eq 0 }
+        its(:stderr) { should eq '' }
+
+        let(:data) do
+          if subject.exit_status.zero?
+            JSON.parse(subject.stdout)
+          else
+            {}
+          end
+        end
+
         describe "#{base_project_sa[environment_code][business_unit]} IAM Policy" do
           it 'should exist' do
             expect(data).to_not be_empty
@@ -205,9 +217,10 @@ control 'gcloud-projects' do
             expect(data['bindings'][0]['members']).to include(
               "serviceAccount:#{base_project_sa[environment_code][business_unit]}"
             )
-            expect(data['bindings'][0]['role']).to eq "roles/editor"
+            expect(data['role']).to eq "roles/editor"
           end
         end
+      end
 
       describe command("gcloud compute shared-vpc get-host-project #{restricted_projects_id[environment_code][business_unit]} --format=json") do
         its(:exit_status) { should eq 0 }
